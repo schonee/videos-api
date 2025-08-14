6fa4196b608ce1962915780e938b756aa1122267
Thalia Schöne <thalia.schone@gmail.com>
2025-08-13 22:27:29 -0300

    feat: crud and in memory db

--- server.js
+++ server.js
@@ -1,22 +1,49 @@
 import { fastify } from 'fastify';
-
+import { DatabaseMemory } from './database-memory.js';
 
 const server = fastify();
+const database = new DatabaseMemory();
 
-server.post('/videos', () => {
-  return "Hello"
+server.post('/videos', (request, reply) => {
+
+  const { title, description, duration } = request.body;
+
+  database.create({
+    title,
+    description,
+    duration,
+  })
+
+  return reply.status(201).send();
 });
 
-server.get('/videos', () => {
-  return "hello get"
+server.get('/videos', (request) => {
+
+  const search = request.query.search;
+
+  const videos = database.list(search);
+  return videos;
 })
 
-server.put('/videos/:id', () => {
-  return "hello node"
+server.put('/videos/:id', (request, reply) => {
+  const videoId = request.params.id;
+
+  const { title, description, duration } = request.body;
+  database.update(videoId, {
+    title,
+    description,
+    duration
+  })
+
+  return reply.status(204).send();
 })
 
-server.delete('/videos/:id', () => {
-  return "hello get"
+server.delete('/videos/:id', (request, reply) => {
+  const videoId = request.params.id;
+
+  database.delete(videoId);
+
+  return reply.status(204).send();
 })
 
 server.listen({ port: 3333 })
